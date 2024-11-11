1112dd60c624013db4ff308fd5e4433fc004bfad
Krasser Roland <roland.krasser@a1.at>
2024-11-11 11:07:20 +0100

    wordle-plot

--- wordle.R
+++ wordle.R
@@ -17,15 +17,45 @@ data |> explore(rare, target = try, color = mix_color("lightblue", "coral", n = 
 data |> explain_tree(target = player)
 data |> explain_tree(target = try)
 
-data_ori |> 
-  mutate(diff = try_player_a - try_player_b) |> 
-  add_var_id("round") |> 
-  mutate(diff_cum = cumsum(diff)) |> 
-  ggplot(aes(round, diff_cum)) +
-    geom_line(color = "red", linewidth = 2) +
-    theme_minimal()
+data_all <- data_ori |> 
+  add_var_id(name = "round") |> 
+  mutate(diff = try_player_b - try_player_a) |> 
+  mutate(diff_cum = cumsum(diff))
+  
+data_1 <- data_all |> filter(round < 20)   
+data_2 <- data_all |> filter(round >= 20)   
+data_3 <- data_all |> filter(round >= 40)   
 
-word_vec <- data_ori$word
+color_1 <- "#888888" # "#FFAAAA"
+color_2 <- "#c8b359" # "#FFAAAA"
+color_3 <- "#67a760" # "red"
+  
+
+data_all |> 
+  ggplot(aes(round, diff_cum)) +
+    geom_line(color = color_1, linewidth = 2) +
+    geom_line(data = data_2, color =  color_2, linewidth = 2) +
+    geom_line(data = data_3, color =  color_3, linewidth = 2) +
+    ylab("me better (cummulated)") +
+    ggtitle("What you can learn from playing WORDLE") +
+    labs(subtitle = "Against a Data Scientist (me)\n") +
+    theme_minimal() +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    theme(plot.subtitle = element_text(hjust = 0.5)) +
+    geom_vline(xintercept = 20, color = "darkgrey", linetype = "dashed", linewidth = 0.8, alpha = 0.5) +
+    geom_vline(xintercept = 40, color = "darkgrey", linetype = "dashed", linewidth = 0.8, alpha = 0.5) +
+    geom_hline(yintercept = 14, color = "darkgrey", linetype = "dashed", linewidth = 0.8, alpha = 0.5) +
+    geom_text(x=10, y=23.2, color = color_1, size = 3.5,
+              label = "just use logic & intuation\nstart collecting data") +
+    geom_text(x=30, y=23.2, color = color_2, size = 3.5,
+            label = "your opponent\nwill catch up") +
+    geom_text(x=50, y=23.2, color = color_3, size = 3.5,
+          label = "use insights\nfrom data") 
+
+  
+
+
+    word_vec <- data_ori$word
 word_score("autor", word_vec)   ##99  #113
 word_score("eiter", word_vec)   ##132 #143
 word_score("einer", word_vec)   ##124 #135
